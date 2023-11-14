6f545794a37fedf771278b0aaa4c017eca7c5476
Norsefey <129870643+Norsefey@users.noreply.github.com>
2023-11-14 17:47:00 -0500

    Ambiance

---
+++ Assets/Scripts/Player/FlashLight.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{//player can turn their flashlight on and off
+    [SerializeField]//refrence to flashlight
+    private GameObject flashlight;
+
+    private bool flashOn = false;
+    // Start is called before the first frame update
+   
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && flashlight != null)//when player inputs flash light with turn off or on depending if it on/off
+        {
+            if (flashOn)
+            {
+                flashlight.SetActive(false);
+                flashOn = false;
+            }
+            else
+            {
+                flashlight.SetActive(true);
+                flashOn = true;
+            }
+            
+        }
+    }
+}
